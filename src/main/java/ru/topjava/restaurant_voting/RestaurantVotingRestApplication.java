1884b8c9e4f0406146eb3ec97146cf5cad8d8609
Vadim Miheev <mnixav@gmail.com>
2022-06-01 20:21:24 +0300

    Sping Boot entry point;

---
+++ src/main/java/ru/topjava/restaurant_voting/RestaurantVotingRestApplication.java
@@ -0,0 +1,12 @@
+package ru.topjava.restaurant_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingRestApplication.class, args);
+    }
+}
