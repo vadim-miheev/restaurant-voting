984b37cc47dc33827dd0f5289afcf6bd00abb95e
Vadim Miheev <mnixav@gmail.com>
2022-06-16 20:33:55 +0300

    Additional DB init data;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,28 @@
-INSERT INTO users (name, login, password)
+INSERT INTO USERS (NAME, LOGIN, PASSWORD)
 VALUES ('User', 'user', '{noop}user'),
        ('Admin', 'admin', '{noop}admin'),
        ('Guest', 'guest', '{noop}guest');
+
+INSERT INTO RESTAURANTS (NAME)
+VALUES ('restaurant1'),
+       ('restaurant2'),
+       ('restaurant3'),
+       ('restaurant4');
+
+INSERT INTO MENUS (RESTAURANT_ID, DATE)
+VALUES (1, '2022-06-16'),
+       (2, '2022-06-15'),
+       (3, CURRENT_DATE);
+
+INSERT INTO DISHES (NAME, PRICE_IN_CENTS, MENU_ID)
+VALUES ('dish1', 485, 1),
+       ('dish2', 780, 1),
+       ('dish3', 880, 1),
+       ('dish4', 740, 2),
+       ('dish5', 340, 2),
+       ('dish6', 660, 2),
+       ('dish7', 760, 2),
+       ('dish8', 530, 3),
+       ('dish9', 675, 3),
+       ('dish10', 330, 3),
+       ('dish11', 545, 3);
