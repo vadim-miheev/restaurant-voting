cee5da85909fea52dba7309e276a1d217185a897
Vadim Miheev <mnixav@gmail.com>
2022-07-03 18:28:20 +0300

    Json Utility;

--- src/main/java/ru/topjava/restaurant_voting/config/AppConfig.java
+++ src/main/java/ru/topjava/restaurant_voting/config/AppConfig.java
@@ -1,9 +1,12 @@
 package ru.topjava.restaurant_voting.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.topjava.restaurant_voting.util.JsonUtil;
 
 import java.sql.SQLException;
 
@@ -15,5 +18,10 @@ public class AppConfig {
     Server h2Server() throws SQLException {
         log.info("Start H2 TCP server");
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+
+    @Autowired
+    private void setMapper(ObjectMapper objectMapper) {
+        JsonUtil.setMapper(objectMapper);
     }
 }
