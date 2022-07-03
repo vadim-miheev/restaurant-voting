4fc884de96e3220b8b03ea8518aea565a360bdcb
Vadim Miheev <mnixav@gmail.com>
2022-07-03 18:22:04 +0300

    RestaurantUtil.java refactoring;

--- src/main/java/ru/topjava/restaurant_voting/util/RestaurantUtil.java
+++ src/main/java/ru/topjava/restaurant_voting/util/RestaurantUtil.java
@@ -1,12 +1,14 @@
 package ru.topjava.restaurant_voting.util;
 
+import lombok.experimental.UtilityClass;
 import ru.topjava.restaurant_voting.model.Restaurant;
-import ru.topjava.restaurant_voting.to.RestaurantTo;
+import ru.topjava.restaurant_voting.dto.RestaurantTo;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@UtilityClass
 public class RestaurantUtil {
     public static List<RestaurantTo> getTos(Collection<Restaurant> restaurants) {
         return restaurants.stream()
