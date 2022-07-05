bbd49473e5b82cafb4ad1d57b274242af0d4bf3d
Vadim Miheev <mnixav@gmail.com>
2022-07-05 10:23:02 +0300

    AdminRestaurantController - get();

--- src/main/java/ru/topjava/restaurant_voting/repository/RestaurantRepository.java
+++ src/main/java/ru/topjava/restaurant_voting/repository/RestaurantRepository.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Repository;
 import ru.topjava.restaurant_voting.model.Restaurant;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
     @Query("SELECT r FROM Restaurant r JOIN FETCH r.menus m WHERE m.date = CURRENT_DATE()")
     List<Restaurant> getAllWithMenuForToday();
+
+    @Query("SELECT r FROM Restaurant r JOIN FETCH r.menus m WHERE r.id = :restaurantId")
+    Optional<Restaurant> getWithMenus(int restaurantId);
 }
--- src/main/java/ru/topjava/restaurant_voting/web/restaurant/AdminRestaurantController.java
+++ src/main/java/ru/topjava/restaurant_voting/web/restaurant/AdminRestaurantController.java
@@ -3,10 +3,13 @@ package ru.topjava.restaurant_voting.web.restaurant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.topjava.restaurant_voting.dto.RestaurantTo;
+import ru.topjava.restaurant_voting.model.Restaurant;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,11 @@ public class AdminRestaurantController extends AbstractRestaurantController {
     @GetMapping("/today")
     List<RestaurantTo> getAllWithMenuForToday() {
         return super.getAllWithMenuForToday();
+    }
+
+    @GetMapping("/{id}")
+    Restaurant get(@PathVariable int id) {
+        return restaurantRepository.getWithMenus(id).orElseThrow(
+                () -> new EntityNotFoundException("There is no restaurant with such id"));
     }
 }
