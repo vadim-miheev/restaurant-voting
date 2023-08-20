faff004cb0eb6808e1aab561e9e95f48be8ba24c
Vadim Miheev <mnixav@gmail.com>
2023-08-20 16:30:20 +0300

    AppException.java - get status fix;

--- src/main/java/ru/topjava/restaurant_voting/error/AppException.java
+++ src/main/java/ru/topjava/restaurant_voting/error/AppException.java
@@ -8,14 +8,18 @@ import org.springframework.web.server.ResponseStatusException;
 @Getter
 public class AppException extends ResponseStatusException {
     private final ErrorAttributeOptions options;
+    private final HttpStatus status;
 
     public AppException(HttpStatus status, String message, ErrorAttributeOptions options) {
         super(status, message);
         this.options = options;
+        this.status = status;
     }
 
     @Override
     public String getMessage() {
         return getReason();
     }
+
+    public HttpStatus getStatus() { return this.status; }
 }
