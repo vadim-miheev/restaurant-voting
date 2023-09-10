37e9664008fdfb6f595f2664ed098ddca477e904
Vadim Miheev <mnixav@gmail.com>
2023-09-10 12:40:35 +0300

    ObjectMapper reverting;

--- src/main/java/ru/topjava/restaurant_voting/config/AppConfig.java
+++ src/main/java/ru/topjava/restaurant_voting/config/AppConfig.java
@@ -1,22 +1,14 @@
 package ru.topjava.restaurant_voting.config;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ProblemDetail;
 import ru.topjava.restaurant_voting.util.JsonUtil;
 
 import java.sql.SQLException;
-import java.util.Map;
-
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @Configuration
 @Slf4j
@@ -28,18 +20,8 @@ public class AppConfig {
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 
-    //   https://stackoverflow.com/a/74630129/548473
-    @JsonAutoDetect(fieldVisibility = NONE, getterVisibility = ANY)
-    interface MixIn {
-        @JsonAnyGetter
-        Map<String, Object> getProperties();
-    }
-
     @Autowired
-    void configureAndStoreObjectMapper(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new Hibernate5JakartaModule());
-        // ErrorHandling: https://stackoverflow.com/questions/7421474/548473
-        objectMapper.addMixIn(ProblemDetail.class, MixIn.class);
+    private void setMapper(ObjectMapper objectMapper) {
         JsonUtil.setMapper(objectMapper);
     }
 }
